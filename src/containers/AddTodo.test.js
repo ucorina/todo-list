d116e544772ba2918c8f8b2e9914015e4cbf8260
Corina Udrescu <corinutz@gmail.com>
2019-05-29 18:04:56 +0200

    Add Enzyme; Add test for AddTodo container

---
+++ src/containers/AddTodo.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import configureMockStore from "redux-mock-store";
+import { shallow } from "enzyme";
+import AddTodo from "./AddTodo";
+
+const mockStore = configureMockStore();
+
+describe("AddTodo", () => {
+  let store, wrapper;
+
+  beforeEach(() => {
+    const initialState = { todos: [] };
+    store = mockStore(initialState);
+    expect(store.getState()).toEqual({ todos: [] });
+    wrapper = shallow(<AddTodo store={store} />);
+  });
+
+  it("adds a todo", () => {
+    const todo = {
+      text: "always test your code",
+      priority: 1
+    };
+    wrapper.simulate("itemAdded", todo);
+
+    const actions = store.getActions();
+    expect(actions).toEqual([{ type: "ADD_TODO", ...todo }]);
+  });
+});
