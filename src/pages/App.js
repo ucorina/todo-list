0ee3a1477bb9ab56bcd94b20707b3764b15a6f82
Corina Udrescu <corinutz@gmail.com>
2019-05-30 13:14:01 +0200

    Remove leftover Router code

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { makeStyles } from "@material-ui/core/styles";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "../components/Header";
 import TodoListPage from "../pages/TodoListPage";
-import LoginPage from "../pages/LoginPage";
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -18,16 +16,15 @@ const useStyles = makeStyles(theme => ({
 function App() {
   const classes = useStyles();
   return (
-    <Router>
+    <div>
       <Header />
       <Container maxWidth="md" className={classes.title}>
         <CssBaseline />
         <main>
-          <Route path="/" exact component={TodoListPage} />
-          <Route path="/login" component={LoginPage} />
+          <TodoListPage />
         </main>
       </Container>
-    </Router>
+    </div>
   );
 }
 
